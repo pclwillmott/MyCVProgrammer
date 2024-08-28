0c50ff4cec5eb63c57202ac68b1d272156c3d98b
pclwillmott <pwillmott@northrock.bm>
2024-08-28 12:22:25 -0300

    Initial Commit

---
+++ MyCVProgrammer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MyCVProgrammer
+//
+//  Created by Paul Willmott on 28/08/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ MyCVProgrammer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyCVProgrammer
+//
+//  Created by Paul Willmott on 28/08/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
